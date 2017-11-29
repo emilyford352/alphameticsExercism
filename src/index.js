f930da0acc6f10f7a5b1863aec8b702d16858b0b
Emily Ford <emilyford352@gmail.com>
2017-11-28 23:09:59 -0500

    Committing function setup

--- src/index.js
+++ src/index.js
@@ -1,26 +1,58 @@
 main();
 
 function main() {
-    var inputString = 'A == B';
+    var inputString = 'AS + A == MOM';
     inputString = inputString.split(' == ');
     var leftSide = parseLeftSide(inputString[0]);
     var rightSide = inputString[1];
+
     if (inequality(leftSide, rightSide)) {
         return null;
     }
-    var totalMax = findMaxTotal(rightSide);
-    var totalMin = findMinTotal(rightSide);
+
+    if (checkForLeadingZero(rightSide, leftSide)) {
+        return null;
+    }
+
+    iterator(inputString, leftSide, rightSide);
+}
+
+function iterator(inputString, leftSide, rightSide) {
+    //var totalMax = findMaxTotal(rightSide);
+    //var totalMin = findMinTotal(rightSide);
+    var uniqueVariables = getUniqueVariables(inputString);
+    //var possibleValues =
+    console.log(uniqueVariables);
+}
+
+function getUniqueVariables(inputString) {
+    inputString[0] = inputString[0].split(' + ');
+    var uniqueVariables = inputString.join('').replace(',', '').split('');
+    var filteredArray = uniqueVariables.filter(function(item, pos){
+        return uniqueVariables.indexOf(item)== pos;
+    });
+    return filteredArray;
+}
+
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
 }
 
 function inequality(leftSide, rightSide) {
-    if (leftSide.length === rightSide.length && leftSide !== rightSide) {
+    if (leftSide.length == 1 && leftSide !== rightSide) {
         return true;
     }
 }
-function checkForBiggerRightSide() {
 
+function checkForLeadingZero(rightSide, leftSide) {
+    var returnValue;
+    leftSide.forEach(function(object) {
+        if (object.length > rightSide.length) {
+            returnValue = true;
+        }
+    });
+    return returnValue;
 }
-
 
 function parseLeftSide(leftSide) {
     return leftSide.split(' + ');
